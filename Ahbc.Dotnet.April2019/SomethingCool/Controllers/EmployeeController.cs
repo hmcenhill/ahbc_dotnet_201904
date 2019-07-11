4e33becbf5eba8f8bb811e2c3a118fb7f7a103a9
Jason Robert <jason@grandcircus.co>
2019-07-11 19:37:13 -0400

    Adding mock database

--- Ahbc.Dotnet.April2019/SomethingCool/Controllers/EmployeeController.cs
+++ Ahbc.Dotnet.April2019/SomethingCool/Controllers/EmployeeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SomethingCool.Data;
 using SomethingCool.Models;
 
 namespace SomethingCool.Controllers
 {
     public class EmployeeController : Controller
     {
+        private static DataRepository _repository = new DataRepository();
+
         public IActionResult Index()
         {
             return View();
@@ -16,18 +19,16 @@ namespace SomethingCool.Controllers
 
         public IActionResult Edit(int id)
         {
-            // Call database here
-            // Instead lets create an employee manually
-            var employee = new Employee
-            {
-                Id = 1,
-                FirstName = "Jason",
-                LastName = "Robert",
-                Age = 35,
-                Email = "jason@grandcircus.co"
-            };
-
+            var employee = _repository.Get(id);
             return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Employee employee)
+        {
+            _repository.Save(id, employee);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Ahbc.Dotnet.April2019/SomethingCool/Data/DataRepository.cs
@@ -0,0 +1,43 @@
+﻿using SomethingCool.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SomethingCool.Data
+{
+    public class DataRepository
+    {
+        private readonly Dictionary<int, Employee> _data =
+            new Dictionary<int, Employee>
+            {
+                { 5, new Employee
+                    {
+                        Id = 5,
+                        FirstName = "Jason",
+                        LastName = "Robert",
+                        Age = 35,
+                        Email = "jason@grandcircus.co"
+                    }}
+            };
+
+        public Employee Get(int id)
+        {
+            if (_data.TryGetValue(id, out var employee))
+            {
+                return employee;
+            }
+
+            return null;
+        }
+
+        public void Add(Employee employee)
+        {
+            var id = _data.Keys.Max() + 1;
+            _data[id] = employee;
+        }
+
+        public void Save(int id, Employee employee)
+        {
+            _data[id] = employee;
+        }
+    }
+}
