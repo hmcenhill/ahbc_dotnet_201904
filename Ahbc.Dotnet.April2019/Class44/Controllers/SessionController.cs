3e4a03bb6b18a0eac7afbdba6ffea5c3b04ecfdc
Jason Robert <jason@grandcircus.co>
2019-07-17 19:14:49 -0400

    Class 44 - ViewBag / ViewData

---
+++ Ahbc.Dotnet.April2019/Class44/Controllers/SessionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class44.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewDataExample()
+        {
+            ViewData["Ticks"] = DateTime.Now.Ticks;
+            return RedirectToAction("ViewDataBad");
+        }
+
+        public IActionResult ViewDataBad()
+        {
+            return View();
+        }
+
+        public IActionResult ViewBagExample()
+        {
+            ViewBag.Ticks = DateTime.Now.Ticks;
+            return View();
+        }
+    }
+}
