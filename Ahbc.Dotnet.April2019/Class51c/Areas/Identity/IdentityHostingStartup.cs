f7ae86231a3ec51cbbe85af85005a7cb755e086a
Jason Robert <jason@grandcircus.co>
2019-08-05 19:00:20 -0400

    Extra sample Identity Projects

---
+++ Ahbc.Dotnet.April2019/Class51c/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Class51c.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Class51c.Areas.Identity.IdentityHostingStartup))]
+namespace Class51c.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<IdentityContext>();
+            });
+        }
+    }
+}
