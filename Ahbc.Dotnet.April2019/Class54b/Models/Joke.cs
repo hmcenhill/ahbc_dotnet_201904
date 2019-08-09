5863cd99901b04846be7cd76ec0be2e02462f804
Jason Robert <jason@grandcircus.co>
2019-08-08 21:21:04 -0400

    Star Wars example

---
+++ Ahbc.Dotnet.April2019/Class54b/Models/Joke.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class54b.Models
+{
+
+    public class Joke
+    {
+        public string Type { get; set; }
+        public Value Value { get; set; }
+    }
+
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Joke { get; set; }
+        public string[] Categories { get; set; }
+    }
+
+}
